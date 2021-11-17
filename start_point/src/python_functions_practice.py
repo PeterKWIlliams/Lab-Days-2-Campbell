6bb04a2f3a56d1a8f2ab7dc22f94a22b9c02d43c
peterwilliams <peterkennethwilliams@gmail.com>
2021-11-17 12:06:13 +0000

    commit 1 pair programming 2

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,25 @@
+def return_10():
+    return 10
+
+def add(num_1,num_2):
+    return num_1 + num_2
+
+def subtract(num_1,num_2):
+    return num_1 - num_2
+
+def multiply(num_1,num_2):
+    return num_1 * num_2
+
+def divide(num_1,num_2):
+    return num_1 / num_2 
+
+def length_of_string(str_length):
+    return len(str_length) 
+
+def join_string(str_1,str_2):
+    return str_1 + str_2    
+
+def  add_string_as_number(str_1,str_2):
+    return int(str_1) + int(str_2)
+
+def number_to_full_month_name()    
