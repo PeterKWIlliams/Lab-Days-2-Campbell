8d91cfe6ac87e363b4809340a074480fd81b40ae
peterwilliams <peterkennethwilliams@gmail.com>
2021-11-17 12:27:24 +0000

    fourth commit

--- start_point/src/python_functions_practice.py
+++ start_point/src/python_functions_practice.py
@@ -39,6 +39,21 @@ def number_to_full_month_name(month):
     }
     return months[month]
 
-
+def number_to_short_month_name(month):
+    months = {
+        1:"Jan",
+        2:"Feb",
+        3:"Mar",
+        4:"Apr",
+        5:"May",
+        6:"Jun",
+        7:"Jul",
+        8:"Aug",
+        9:"Sep",
+        10:"Oct",
+        11:"Nov",
+        12:"Dec"
+    }
+    return months[month]
 
     
