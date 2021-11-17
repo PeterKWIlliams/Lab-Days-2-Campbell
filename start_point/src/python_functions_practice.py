c875e16f27aae462ca5c42f9efcb356e4410f3a2
peterwilliams <peterkennethwilliams@gmail.com>
2021-11-17 12:22:37 +0000

    3rd commit

--- start_point/src/python_functions_practice.py
+++ start_point/src/python_functions_practice.py
@@ -24,10 +24,21 @@ def  add_string_as_number(str_1,str_2):
 
 def number_to_full_month_name(month):
     months = {
-        1:"january"
-        }
+        1:"January",
+        2:"February",
+        3:"March",
+        4:"April",
+        5:"May",
+        6:"June",
+        7:"July",
+        8:"August",
+        9:"September",
+        10:"October",
+        11:"November",
+        12:"December"
+    }
     return months[month]
 
-def     
+
 
     
